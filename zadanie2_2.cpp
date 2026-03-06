4285b42e7319ef681b95d216917196ef2d4f7e27
Anna Rozanska <arozanska987@gmail.com>
2026-03-06 18:11:20 +0100

    Dodanie plików

---
+++ zadanie2_2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello world!" << endl;
+    cout << "Drugi tekst" << endl;
+    cout << "Trzeci tekst" << endl;
+    return 0;
+}
