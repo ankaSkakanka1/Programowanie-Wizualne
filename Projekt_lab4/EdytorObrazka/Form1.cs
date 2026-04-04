f2cdc119ee70810f7993883797b9a7ab95d5604d
Anna Rozanska <arozanska987@gmail.com>
2026-04-04 13:07:08 +0200

    Rotate Commit

--- Projekt_lab4/EdytorObrazka/Form1.cs
+++ Projekt_lab4/EdytorObrazka/Form1.cs
@@ -7,6 +7,8 @@ public partial class Form1 : Form
 {
     private PictureBox pictureBox;
     private Button btnLoad;
+    private RadioButton rb90, rb180, rb270;
+    private Button btnRotate;
     public Form1()
     {
         InitializeComponent();
@@ -23,6 +25,14 @@ public partial class Form1 : Form
         btnLoad.Location = new Point(20, 400); 
         btnLoad.Click += (s, e) => loadimage();
         this.Controls.Add(btnLoad);
+
+        rb90 = new RadioButton { Text = "90°", Location = new Point(20, 50), Checked = true };
+        rb180 = new RadioButton { Text = "180°", Location = new Point(20, 80) };
+        rb270 = new RadioButton { Text = "270°", Location = new Point(20, 110) };
+
+        btnRotate = new Button { Text = "Rotate", Location = new Point(20, 150), BackColor = Color.LightPink };
+        btnRotate.Click += (s, e) => HandleRotation();
+        this.Controls.AddRange(new Control[] { rb90, rb180, rb270, btnRotate });
 
         pictureBox = new PictureBox();
         pictureBox.Location = new Point(150, 20);
@@ -41,5 +51,32 @@ public partial class Form1 : Form
         {
             pictureBox.Image = new Bitmap(openFileDialog.FileName);
         }
+    }
+
+    private void HandleRotation()
+    {
+        if (pictureBox.Image == null) return;
+
+        if (rb90.Checked) rotateImage90();
+        else if (rb180.Checked) rotateImage180();
+        else if (rb270.Checked) rotateImage270();
+    }
+
+    private void rotateImage90()
+    {
+        pictureBox.Image.RotateFlip(RotateFlipType.Rotate90FlipNone);
+        pictureBox.Refresh();
+    }
+
+    private void rotateImage180()
+    {
+        pictureBox.Image.RotateFlip(RotateFlipType.Rotate180FlipNone);
+        pictureBox.Refresh();
+    }
+
+    private void rotateImage270() 
+    {
+        pictureBox.Image.RotateFlip(RotateFlipType.Rotate270FlipNone);
+        pictureBox.Refresh();
     }
 }
--- Projekt_lab4/EdytorObrazka/obj/Debug/net10.0-windows/EdytorObrazka.AssemblyInfo.cs
+++ Projekt_lab4/EdytorObrazka/obj/Debug/net10.0-windows/EdytorObrazka.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EdytorObrazka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e5b1c1fdf0d2c4937079e8b16dc8db1eb1855e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76c7c4aba010a041a3022a4cebf020e253662dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("EdytorObrazka")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EdytorObrazka")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
