58d9d87c7bd8e94af0a221e373a72a9b36b5906b
Anna Rozanska <arozanska987@gmail.com>
2026-04-16 17:18:59 +0200

    Form1

--- Projekt_lab6/ProstaGra/Form1.cs
+++ Projekt_lab6/ProstaGra/Form1.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Windows.Forms;
 namespace ProstaGra;
 
 public partial class Form1 : Form
@@ -6,4 +8,22 @@ public partial class Form1 : Form
     {
         InitializeComponent();
     }
+
+     private void btnStart_Click(object sender, EventArgs e)
+        {
+            int width = (int)numWidth.Value;
+            int height = (int)numHeight.Value;
+            int time = (int)numTime.Value;
+            int dydelfs = (int)numDydelf.Value;
+            int raccoons = (int)numRaccoon.Value;
+            int crocs = (int)numCroc.Value;
+
+            OknoGry game = new OknoGry(width, height, time, dydelfs, raccoons, crocs);
+            game.Show();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
 }
